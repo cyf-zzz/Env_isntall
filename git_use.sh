2616d3ab64678967686dbc7565641c3cadc552fa
cyf-zzz <2061048812@qq.com>
2023-03-28 13:14:30 +0800

    git使用

---
+++ git_use.sh
@@ -0,0 +1,23 @@
+# Git 命令使用
+echo "Git 命令使用"
+exit 0
+
+# git 拉取远程分支
+git pull origin master
+
+# git 拉取远程更新
+git fetch origin master
+
+# git 提交本地分支
+git add *
+git commit -m "commit message"
+git push origin master
+
+# git 查看分支
+git branch
+
+# git 切换分支
+git checkout branch_name
+
+# git 创建分支
+git branch branch_name
